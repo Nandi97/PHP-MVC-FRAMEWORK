31e93313cce2a822c09c19da4f48174275340c35
Alvin Maina <cartezalvin@gmail.com>
2021-09-24 11:55:57 +0300

    comment placeholder now works

--- core/Router.php
+++ core/Router.php
@@ -51,11 +51,21 @@ class Router
   public function renderView($view)
   {
     $layoutContent = $this->layoutContent();
-    include_once Application::$ROOT_DIR."/views/$view.php";
+    $viewContent = $this->renderOnlyView($view);
+    return str_replace('{{content}}', $viewContent, $layoutContent);
   }
 
   protected function layoutContent()
   {
-include-include_once Application::$ROOT_DIR."/views/layouts/main.php";
+    ob_start();
+    include_once Application::$ROOT_DIR . "/views/layouts/main.php";
+    return ob_get_clean();
+  }
+
+  protected function renderOnlyView($view)
+  {
+    ob_start();
+    include_once Application::$ROOT_DIR . "/views/$view.php";
+    return ob_get_clean();
   }
 }
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -9,7 +9,8 @@
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
-  {{content}}
+  <title>Hello, world!</title>
+
 </head>
 
 <body>
@@ -31,7 +32,8 @@
       </div>
     </div>
   </nav>
-  <h1>Hello, world!</h1>
+
+  {{content}}
 
   <!-- Optional JavaScript; choose one of the two! -->
 
